8e93ae7402437f0bba458d2670d13ef8fb034e38
soukainanachib <soukaina.nachib23@gmail.com>
2024-07-23 00:06:26 +0100

    done

--- 0x00-Webpack/task_3/modules/body/body.js
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -1,13 +1,11 @@
+import './body.css';
 const $ = require('jquery');
 const _ = require('lodash');
 
 let counter = 0;
 
-$("<p>Holberton Dashboard</p>").appendTo('body');
-$("<p>Dashboard data for the students</p>").appendTo('body');
 $("<button>Click here to get started</button>").appendTo('body');
 $("<p id='count'></p>").appendTo('body');
-$("<p>Copyright - Holberton School</p>").appendTo('body');
 
 function updateCounter() {
   counter++;
