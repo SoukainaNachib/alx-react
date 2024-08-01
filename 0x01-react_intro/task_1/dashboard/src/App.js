8220a9ef70e071fbaf902dd778cb4e002f1fceea
soukainanachib <soukaina.nachib23@gmail.com>
2024-08-01 20:53:21 +0100

    done

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,5 +1,6 @@
 import logo from './holberton-logo.jpg';
 import './App.css';
+import { getFooterCopy, getFullYear } from './utilis';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <p>Login to access the full dashboard</p>
       </body>
       <footer className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+        <p>{`${getFooterCopy(true)} - ${getFullYear()}`}</p>
       </footer>
     </div>
   );
